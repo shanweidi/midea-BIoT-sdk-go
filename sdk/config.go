dfa1fd8057c6cafe95535ac858a407c8fe07dd80
shanwd <shanwd@midea.com>
2023-08-29 16:36:35 +0800

    fix:verify

--- sdk/config.go
+++ sdk/config.go
@@ -53,7 +53,7 @@ func (c *Config) verify() tools.Error {
 	if len(c.Key) == 0 {
 		return tools.NewSdkError(tools.ConfigKeyErrorCode, tools.ConfigKeyErrorMessage, nil)
 	}
-	if c.Protocol != "MQTT" {
+	if c.Protocol != MQTT && c.Protocol != MQTTS {
 		return tools.NewSdkError(tools.ConfigProtocolErrorCode, tools.ConfigProtocolErrorMessage, nil)
 	}
 	return nil
